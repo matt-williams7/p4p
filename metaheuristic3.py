5dc1898fe6b041ab27b960f1f3d8592b14c06629
Matthew Williams <mattwilliams@wifi-172-23-121-101.uoa-wifi.auckland.ac.nz>
2022-09-20 11:42:22 +1200

    Made 'if' statements to make code more robust

--- metaheuristic3.py
+++ metaheuristic3.py
@@ -1,4 +1,5 @@
 # Imports
+from pickle import TRUE
 import pandas as pd
 from pyswarm import pso
 import matplotlib.pyplot as plt
@@ -8,27 +9,31 @@ import numpy as np
 
 # Define fitness function
 
-# Read in data
-data = pd.read_csv("TI_data.csv", index_col='date', parse_dates=True)
-data.sort_index(inplace=True)
 
-# Modify technical indicator columns
-temp1 = data["Aroon up"]==100
-temp2 =data["Aroon down"]==100
-data["Aroon2"] = temp1.astype(int)-temp2.astype(int)
-data["MACD2"] = data["MACD hist"]/abs(data["MACD hist"])
-temp1 = data["RSI"]<30
-temp2 = data["RSI"]>70
-data["RSI2"] =  temp1.astype(int) - temp2.astype(int) 
-data["ADX2"] = data["ADX"]>40
+data_read = True
 
-# Modify close, LR, PR, OBV
-data["OBV"] = (data["OBV"] - np.mean(data["OBV"]))/np.std(data["OBV"])
+if data_read == True:
+    # Read in data
+    data = pd.read_csv("TI_data.csv", index_col='date', parse_dates=True)
+    data.sort_index(inplace=True)
 
-close = data["close"]
+    # Modify technical indicator columns
+    temp1 = data["Aroon up"]==100
+    temp2 =data["Aroon down"]==100
+    data["Aroon2"] = temp1.astype(int)-temp2.astype(int)
+    data["MACD2"] = data["MACD hist"]/abs(data["MACD hist"])
+    temp1 = data["RSI"]<30
+    temp2 = data["RSI"]>70
+    data["RSI2"] =  temp1.astype(int) - temp2.astype(int) 
+    data["ADX2"] = data["ADX"]>40
 
-drop = ["date.1","open","high","low", "close", "volume", "DI_pos", "DI_neg", "Aroon oscilator", "Aroon down", "Aroon up", "MACD", "MACD hist", "MACD signal", "RSI","ADX"] 
-data.drop(drop, axis=1, inplace=True)
+    # Modify close, LR, PR, OBV
+    data["OBV"] = (data["OBV"] - np.mean(data["OBV"]))/np.std(data["OBV"])
+
+    close = data["close"]
+
+    drop = ["date.1","open","high","low", "close", "volume", "DI_pos", "DI_neg", "Aroon oscilator", "Aroon down", "Aroon up", "MACD", "MACD hist", "MACD signal", "RSI","ADX"] 
+    data.drop(drop, axis=1, inplace=True)
 
 #plt.plot(data.index,data.close)
 #plt.show()
@@ -40,7 +45,7 @@ def sigmoid(x,a):
     return sig
 
 # Define function for taking action for current timestep
-def action(x, current, portfolio, price, fee):
+def action(x, current, portfolio, price, fee,fee_bool):
     sigScale = x[0]
     actionScale = x[1]
     linParams = x[2:]
@@ -73,22 +78,39 @@ def action(x, current, portfolio, price, fee):
     # If buying
     if change>0:
         # If cant afford
-        if change*price+fee > cash:
-            # Buy as much as you can
-            return((coins + cash/(price*(1+fee)),0), price)
-        # Otherwise return change
+        if fee_bool == True:
+            if change*price+fee > cash:
+                # Buy as much as you can
+                return((coins + cash/(price*(1+fee)),0), price)
+            # Otherwise return change
+            else:
+                #print(change)
+                return((coins+change,cash-change*price*(1+fee)), price)
         else:
+            if change*price > cash:
+            # Buy as much as you can
+                return((coins + cash/(price),0), price)
+        # Otherwise return change
+            else:
             #print(change)
-            return((coins+change,cash-change*price*(1+fee)), price)
+                return((coins+change,cash-change*price), price)
     # If selling
     else:
+        if fee_bool == True:
         # If selling more than we have
-        if abs(change)>coins:
-            # No change
-            return((0,cash + coins*price*(1-fee)),price)
-        # Otherwise return change
+            if abs(change)>coins:
+                # No change
+                return((0,cash + coins*price*(1-fee)),price)
+            # Otherwise return change
+            else:
+                return((coins+change, cash-change*price*(1-fee)),price)
         else:
-            return((coins+change, cash-change*price*(1-fee)),price)
+            if abs(change)>coins:
+                # No change
+                return((0,cash + coins*price),price)
+            # Otherwise return change
+            else:
+                return((coins+change, cash-change*price),price)
 
 
 def fitness(x, *args):
@@ -117,7 +139,7 @@ def fitness(x, *args):
             #next = data.iloc[ind+1]
 
             # Take action
-            portfolio,finalPrice = action(x, current, portfolio, close[ind], fee)
+            portfolio,finalPrice = action(x, current, portfolio, close[ind], fee, fee_bool)
             
         changes[month] = (-portfolio[0]+initPort[0])*finalPrice - portfolio[1] + initPort[1]
     print(np.mean(changes))
@@ -135,9 +157,10 @@ lb = [-x for x in ub]
 #lb.extend(negext)
 
 fee = 0.01
-args = (data,(1,close[0]), close, fee)
+fee_bool = TRUE
+args = (data,(1,close[0]), close, fee,fee_bool)
 # Run pso
-#xopt, fopt = pso(fitness, lb, ub, args=args, debug = True, maxiter=8)
+# xopt, fopt = pso(fitness, lb, ub, args=args, debug = True, maxiter=8)
 
 #print(xopt)
 #print(fopt)
@@ -151,7 +174,10 @@ args = (data,(1,close[0]), close, fee)
 
 #xopt = [  710.8384057,   -950.06709355,   -21.51516912,  -963.26508504, -1000, 758.73503134,  -226.24275816]
 
-xopt = [-782.760197,   -18741.76458582, -11033.87422626,  24382.07439214, 9624.6234799,  -70646.61920364, -16002.13638915,  27560.46970967, -45224.23673756]
+# xopt = [-782.760197,   -18741.76458582, -11033.87422626,  24382.07439214, 9624.6234799,  -70646.61920364, -16002.13638915,  27560.46970967, -45224.23673756]
+
+#xopt with fee of 0.1
+xopt = [ 100000, 46005.44563952, -13062.14386056, 19395.57527876, -31810.97743726, -100000, 35960.52434828, 73143.6729333, -86243.24895243]
 
 n = data.shape[0]
 initPort = (1,close[0])
@@ -170,7 +196,7 @@ for ind in range(n):
     #next = data.iloc[ind+1]
 
     # Take action
-    portfolio,finalPrice = action(xopt, current, portfolio, close[ind], fee)
+    portfolio,finalPrice = action(xopt, current, portfolio, close[ind], fee, fee_bool)
     coins = portfolio[0]
     cash = portfolio[1]
     coinss[ind] = coins
@@ -180,6 +206,9 @@ for ind in range(n):
 
 #print(wealths-ogWealths)
 #print(np.mean(wealths-ogWealths))
+
+
+
 dates = data.index
 #plt.plot(dates, cashs, 'g', label = 'Cash')
 plt.plot(dates, wealths, 'b', label = 'Wealth')
