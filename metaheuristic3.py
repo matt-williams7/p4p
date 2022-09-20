2daf63bc03689ee965ff34d4b5df9c33bd7fd7fd
George Perry <gper789@aucklanduni.ac.nz>
2022-09-20 13:34:29 +1200

    Added some more functions and RSI testing functionality

--- metaheuristic3.py
+++ metaheuristic3.py
@@ -5,38 +5,44 @@ from pyswarm import pso
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Read in data
+# FUNCTIONS ====================================
 
-# Define fitness function
+# Define function for reading in data
+def readData(file, RSI =False):
+    ''' 
 
-
-data_read = True
-
-if data_read == True:
+    '''
     # Read in data
-    data = pd.read_csv("TI_data.csv", index_col='date', parse_dates=True)
+    data = pd.read_csv(file, index_col='date', parse_dates=True)
     data.sort_index(inplace=True)
 
     # Modify technical indicator columns
-    temp1 = data["Aroon up"]==100
-    temp2 =data["Aroon down"]==100
-    data["Aroon2"] = temp1.astype(int)-temp2.astype(int)
-    data["MACD2"] = data["MACD hist"]/abs(data["MACD hist"])
+    if (RSI==False):
+        temp1 = data["Aroon up"]==100
+        temp2 =data["Aroon down"]==100
+        data["Aroon2"] = temp1.astype(int)-temp2.astype(int)
+        data["MACD2"] = data["MACD hist"]/abs(data["MACD hist"]) 
+        data["ADX2"] = data["ADX"]>40
+        # Modify close, LR, PR, OBV
+        data["OBV"] = (data["OBV"] - np.mean(data["OBV"]))/np.std(data["OBV"])
+    else:
+        data.drop(["Log Return", "Percentage Return", "OBV"], axis=1, inplace=True)
+
     temp1 = data["RSI"]<30
     temp2 = data["RSI"]>70
-    data["RSI2"] =  temp1.astype(int) - temp2.astype(int) 
-    data["ADX2"] = data["ADX"]>40
+    data["RSI2"] =  temp1.astype(int) - temp2.astype(int)
 
-    # Modify close, LR, PR, OBV
-    data["OBV"] = (data["OBV"] - np.mean(data["OBV"]))/np.std(data["OBV"])
 
+
+    # Extract close vector
     close = data["close"]
 
+    # Drop unused columns
     drop = ["date.1","open","high","low", "close", "volume", "DI_pos", "DI_neg", "Aroon oscilator", "Aroon down", "Aroon up", "MACD", "MACD hist", "MACD signal", "RSI","ADX"] 
     data.drop(drop, axis=1, inplace=True)
 
-#plt.plot(data.index,data.close)
-#plt.show()
+    # Return new dataframe and close vector
+    return (data, close)
 
 # Define function to return shifted sigmoid 
 def sigmoid(x,a):
@@ -112,7 +118,7 @@ def action(x, current, portfolio, price, fee,fee_bool):
             else:
                 return((coins+change, cash-change*price),price)
 
-
+# Define sim-opt fitness function
 def fitness(x, *args):
     '''
     x = params
@@ -145,22 +151,28 @@ def fitness(x, *args):
     print(np.mean(changes))
     return(np.mean(changes))
 
+
+# READ DATA =======================================
+
+# Read in data
+data_read = True
+RSI = True
+if data_read == True:
+    data,close = readData("TI_data.csv", RSI)
+
+# FIT ===============================================
+
 # Make ub and lbs
-
-
-
-ub = [100000 for i in range(9)]
+ub = [1000 for i in range(data.shape[1]+2)]
 lb = [-x for x in ub]
-#ub = [1]
-#lb = [0.1]
-#ub.extend(ext)
-#lb.extend(negext)
 
+# Fee
 fee = 0.01
 fee_bool = TRUE
-args = (data,(1,close[0]), close, fee,fee_bool)
+
 # Run pso
-# xopt, fopt = pso(fitness, lb, ub, args=args, debug = True, maxiter=8)
+args = (data,(1,close[0]), close, fee,fee_bool)
+#xopt, fopt = pso(fitness, lb, ub, args=args, debug = True, maxiter=8)
 
 #print(xopt)
 #print(fopt)
@@ -177,50 +189,70 @@ args = (data,(1,close[0]), close, fee,fee_bool)
 # xopt = [-782.760197,   -18741.76458582, -11033.87422626,  24382.07439214, 9624.6234799,  -70646.61920364, -16002.13638915,  27560.46970967, -45224.23673756]
 
 #xopt with fee of 0.1
-xopt = [ 100000, 46005.44563952, -13062.14386056, 19395.57527876, -31810.97743726, -100000, 35960.52434828, 73143.6729333, -86243.24895243]
+#xopt = [ 100000, 46005.44563952, -13062.14386056, 19395.57527876, -31810.97743726, -100000, 35960.52434828, 73143.6729333, -86243.24895243]
 
-n = data.shape[0]
-initPort = (1,close[0])
-portfolio = initPort
-coinss = np.zeros(n)
-cashs = np.zeros(n)
-wealths = np.zeros(n)
-ogWealths = np.zeros(n)
-for ind in range(n):
-    #portfolio = (1, data.close[month])
-   
+#xopt = [ 14103.19537751, 16143.54669374, -4951.91813066, 52975.42537164, 9014.75928065, -57164.95668836, -62689.76194088, -23160.74809452, 40204.74441736]
 
-    # Get current and next day's data
-    #ind = month*30+day
-    current = data.iloc[ind]
-    #next = data.iloc[ind+1]
+xopt = [-736.66114168,  231.86343178, -307.5781159 ]
 
-    # Take action
-    portfolio,finalPrice = action(xopt, current, portfolio, close[ind], fee, fee_bool)
-    coins = portfolio[0]
-    cash = portfolio[1]
-    coinss[ind] = coins
-    cashs[ind] = cash
-    wealths[ind] = coins*finalPrice + cash
-    ogWealths[ind] = close[0]+close[ind]
+# TEST =====================================================
 
-#print(wealths-ogWealths)
-#print(np.mean(wealths-ogWealths))
+# Set up test simulation
+portfolio = (1,close[0])
 
 
+
+# Define function for testing
+def testSim(data, xopt, portfolio):
+    n = data.shape[0]
+    coinss = np.zeros(n)
+    cashs = np.zeros(n)
+    wealths = np.zeros(n)
+    ogWealths = np.zeros(n)
+    
+
+    for ind in range(n):
+        #portfolio = (1, data.close[month])
+    
+
+        # Get current and next day's data
+        #ind = month*30+day
+        current = data.iloc[ind]
+        #next = data.iloc[ind+1]
+
+        # Take action
+        portfolio,finalPrice = action(xopt, current, portfolio, close[ind], fee, fee_bool)
+
+        # Update current coins and cash
+        coins = portfolio[0] 
+        cash = portfolio[1]
+        # Update history vector of coins, cash, wealth, and baseline wealths
+        coinss[ind] = coins
+        cashs[ind] = cash
+        wealths[ind] = coins*finalPrice + cash
+        ogWealths[ind] = close[0]+close[ind]
+    return(coinss, cashs, wealths, ogWealths)
+
+coinss, cashs, wealths, ogWealths = testSim(data, xopt, portfolio)
+
+
+# PLOT =================================
 
 dates = data.index
-#plt.plot(dates, cashs, 'g', label = 'Cash')
-plt.plot(dates, wealths, 'b', label = 'Wealth')
-plt.plot(dates, ogWealths, 'r', label = "Baseline")
-plt.legend()
-plt.show()
-coinvals = coins*close
-plt.plot(dates, coinss, 'y', label = 'Coins')
-plt.plot(dates, close/np.mean(close), 'r', label = 'Price')
-plt.legend()
-plt.show()
 
+def plotVsBaseline(dates, wealths, ogWealths):
+    plt.plot(dates, wealths, 'b', label = 'Wealth')
+    plt.plot(dates, ogWealths, 'r', label = "Baseline")
+    plt.legend()
+    plt.show()
 
+def plotCoins(dates, coinss, close):
+    plt.plot(dates, coinss, 'y', label = 'Coins')
+    plt.plot(dates, close/np.mean(close), 'r', label = 'Price')
+    plt.legend()
+    plt.show()
+
+plotVsBaseline(dates, wealths, ogWealths)
+plotCoins(dates, coinss, close)
 
 
