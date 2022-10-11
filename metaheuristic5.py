b88b64c9ecbb80bf8e5fc38f26d75262e9f9f4c6
George Perry <gper789@aucklanduni.ac.nz>
2022-10-12 09:29:23 +1300

    MH code with some ML functionality, before big changes to properly include ML stuff

--- metaheuristic5.py
+++ metaheuristic5.py
@@ -44,7 +44,7 @@ def readData(file, bch, RSI =False):
     # Drop rows to have nrows a multiple of batch size
     nrow = data.shape[0]
     data = data.iloc[nrow%bch:nrow]
-    
+    close = close[nrow%bch:nrow]
     # Return new dataframe and close vector
     return (data, close)
 
@@ -195,7 +195,7 @@ def testSim(data, xopt, portfolio, nstart, nfinish, fee):
         current = data.iloc[ind]
 
         # Take action
-        portfolio,finalPrice = action(xopt, current, portfolio, close[ind], fee)
+        portfolio,finalPrice,act = action(xopt, current, portfolio, close[ind], fee)
 
         # Update current coins and cash
         coins = portfolio[0] 
@@ -239,19 +239,27 @@ def testSimMonth(data, xopt, portfolio,bch,fee, close):
 
 # Define plotting functions
 def plotVsBaseline(dates, wealths, ogWealths):
+
+
     plt.plot(dates, wealths, 'b', label = 'Wealth')
     plt.plot(dates, ogWealths, 'r', label = "Baseline")
-    plt.title("Autotrader vs Buy&Hold Wealth")
+
+    plt.title("Average Policy Model's vs Buy & Hold Strategy's Wealth")
     plt.ylabel("Wealth ($)")
     plt.xlabel("Date")
     plt.legend()
     plt.show()
 
 def plotCoins(dates, coinss, close):
-    plt.plot(dates, coinss, 'y', label = 'Coins')
-    plt.plot(dates, close/np.mean(close), 'r', label = 'Price')
+
+    fig, ax_left = plt.subplots()
+    ax_right = ax_left.twinx()
+
+    ax_left.plot(dates, coinss, 'y', label = 'Coins')
+    ax_right.plot(dates, close/np.mean(close), 'r', label = 'Price')
+
     plt.xlabel("Date")
-    plt.title("Number of coins vs Normalised coin value")
+    plt.title("Number of coins vs Price")
     plt.legend()
     plt.show()
 
@@ -270,17 +278,43 @@ def plotParams(data, xopt):
     plt.show()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# MAIN CODE ================================================
+data_read = True
+toTrain = False
+toTest = False
+toWrite = False
+toSim = True
 # READ DATA ====================================================
 
 # Read in data
-data_read = True
+
 RSI = False
 bch = 30 # Batch size
 if data_read == True:
-    data,close = readData("TI_data_btc1.csv", bch, RSI)
+    data,close = readData("TI_updated_btc.csv", bch, RSI)
 
+
+
+    pred = pd.read_csv("FILE.csv")
+    data["3daypred"] = pred["Column1"].values[0:data.shape[0]]
+    data["3daypred"] = data["3daypred"]/close
 # Use data after 2020
-data = data.iloc[data.index.year>=2020]
+#data = data.iloc[data.index.year>=2020]
 
 # MAKE TEST/TRAIN ===============================================
 # Get number of days and months in the data
@@ -294,23 +328,23 @@ msamp = rand.sample(ms,nm)
 nmt = int(0.5*nm)
 alltrain = msamp[0:nmt]
 test = msamp[nmt:nm]
-
+fee= 0.01
 
 # TRAIN MODELS ===================================================
-# 
-initPort = (1,close[0])
 
-xopts = []
-perfs = []
+if (toTrain):
+    initPort = (1,close[0])
 
-# Number of models to train
-nmodels = 50
-# Trading fee
-fee = 0.01
-# CVar lamda weight on worst month
-lam = 0.2
+    xopts = []
+    perfs = []
 
-if (True):
+    # Number of models to train
+    nmodels = 50
+    # Trading fee
+    fee = 0.01
+    # CVar lamda weight on worst month
+    lam = 0.2
+
 # For each model iteration
     for i in range(nmodels):
         print("\n\n\n\n\n\n===========================\nModel ", i, "\n======================================\n\n\n\n")
@@ -330,99 +364,221 @@ if (True):
     
         # Run pso
         args = (data, initPort, close, fee, nm, train, lam, bch)
-        xopt, fopt = pso(fitness, lb, ub, args=args, debug = True, maxiter=20)
+        xopt, fopt = pso(fitness, lb, ub, args=args, debug = True, maxiter=12)
 
         # Add model to list
         xopts.append(xopt)
 
-# SIMULATION ============================================
-# Get number of testing months
-nmt = len(test)
+# TESTING SIM ============================================
+xoptsML = pd.read_csv("modelParamsBTCml.csv")
+#xoptsSTD = pd.read_csv("modelParamsBTCall.csv")
+data.drop("3daypred",inplace=True)
+if(toTest): 
+    xopts = pd.read_csv("modelParamsBTCml.csv")
+    nmodels = xopts.shape[1]
+    # Get number of testing months
+    nmt = len(test)
 
-# Initialise lists for monthly performances for baseline, each model, and average model
-base = [] 
-modperfs = []
-average = []
-average2 = []
+    # Initialise lists for monthly performances for baseline, each model, and average model
+    base = [] 
+    modperfs = []
+    average = []
+    average2 = []
 
-# For each testing month
-for i in range(nmt):
-    # Get current month
-    month = test[i]
+    # For each testing month
+    for i in range(nmt):
+        print(str(i+1)+"/"+str(nmt))
+        # Get current month
+        month = test[i]
 
-    # Initialize current average portfolio for two averaging methods
-    avgPort = (1, close[month*bch])
-    avgPort2 = (1, close[month*bch])
-    
-    # List for current portfolio of each model
-    modPorts = [(1, close[month*bch]) for i in range(nmodels)]
-    # List of model's portfolios to average
-    avgPorts = [(1, close[month*bch]) for i in range(nmodels)]
+        # Initialize current average portfolio for two averaging methods
+        avgPort = (1, close[month*bch])
+        avgPort2 = (1, close[month*bch])
+        
+        # List for current portfolio of each model
+        modPorts = [(1, close[month*bch]) for i in range(nmodels)]
+        # List of model's portfolios to average
+        avgPorts = [(1, close[month*bch]) for i in range(nmodels)]
+        # List of model's actions to average
+        modActs = [0 for i in range(nmodels)]
+
+        # Get baseline change in wealth
+
+        # Simulate each model and average model through each month
+        for day in range(bch):
+
+            # Get current day and its data
+            ind = month*bch+day
+            current = data.iloc[ind]
+            price = close[ind]
+
+            # For each model
+            for mod in range(nmodels):
+                # Get current model
+                x = xopts.iloc[:,mod].values
+
+                # Get model's new portfolio from action on current model's portfolio
+                newModPort, fp,act = action(x, current, modPorts[mod], price, fee)
+                modPorts[mod] = newModPort
+
+                # Get model's new portfolio from action on current average portfolio
+                newAvgPort, fp, act = action(x, current, avgPort, price, fee)
+                avgPorts[mod] = newAvgPort
+
+                newAvgPort, fp, act = action(x, current, avgPort2, price, fee)
+                modActs[mod] = act
+
+                
+            
+            # Get average portfolio method 1
+            coin = np.mean([i[0] for i in avgPorts])
+            cash = np.mean([i[1] for i in avgPorts])
+            avgPort = (coin,cash)
+
+            # Get average portfolio method 2
+            meanAct = np.mean(modActs)
+            if(meanAct>0):
+                avgPort2 = (avgPort2[0]+meanAct,avgPort2[1]-meanAct*price*(1+fee))
+            else:
+                avgPort2 = (avgPort2[0]+meanAct, avgPort2[1]-meanAct*price*(1-fee))
+            
+
+        # Add performances ** comparing wealth at end to wealth at start:
+        # (not hold wealth at end to trader wealth a end like above)
+        # update initport to be star of month? or just a sum?
+
+        # Starting wealth
+        iWealth = 2*close[month*bch]
+
+        # Baseline
+        base.append(price - close[month*bch])
+        lis = [por[0]*price + por[1] - iWealth for por in modPorts]
+        modperfs.append(lis)
+        average.append(avgPort[0]*price + avgPort[1]-iWealth)
+        average2.append(avgPort2[0]*price + avgPort2[1]-iWealth)
+
+    # Write to csv =================================================================\
+    if(toWrite):
+        # Baseline and averages performance
+        df1 = pd.DataFrame([base,average,average2])
+        df1 = df1.transpose()
+        df1.columns = ["Baseline", "AveragePort", "AverageAct"]
+        df1.to_csv("base&avgPerfsBTCmlOnAll.csv", index=False)
+
+        # Model performances
+        df2 = pd.DataFrame(modperfs)
+        df2.columns = ["Model"+str(i) for i in range(nmodels)]
+        df2.to_csv("modPerfsBTCmlOnAll.csv", index=False)
+
+        # Models
+        #df3 = pd.DataFrame(xopts)
+        #df3 = df3.transpose()
+        #df3.columns = ["Model"+str(i) for i in range(nmodels)]
+        #df3.to_csv("modelParamsBTCml.csv",index=False)
+
+
+# Define function for simulating average trader on a subset of data
+def avgSim(data, xopts, portfolio, nstart, nfinish, fee,close):
+
+    # Get number of days in simulatio
+    n2 = nfinish-nstart
+
+    # Set up output arrays
+    coinss = np.zeros(n2)
+    cashs = np.zeros(n2)
+    wealths = np.zeros(n2)
+    ogWealths = np.zeros(n2)
+
+    #nmodels = xopts.shape[1]
+    nmodels = len(xopts)
+
+
     # List of model's actions to average
     modActs = [0 for i in range(nmodels)]
 
-    # Get baseline change in wealth
-
-    # Simulate each model and average model through each month
-    for day in range(bch):
-
-        # Get current day and its data
-        ind = month*bch+day
+    # For each day
+    for ind in range(nstart,nfinish):
+        if(ind%10==0):
+            print("Day "+str(ind)+" of "+str(nfinish))
+        # Get current day
         current = data.iloc[ind]
         price = close[ind]
-
         # For each model
         for mod in range(nmodels):
             # Get current model
-            x = xopts[mod]
-
-            # Get model's new portfolio from action on current average portfolio
-            newModPort, fp,act = action(x, current, modPorts[mod], price, fee)
-            modPorts[mod] = newModPort
+            #x = xopts.iloc[:,mod].values
+            x = xopts.iloc[:,mod].values
 
             # Get model's new portfolio from action on current model's portfolio
-            newAvgPort, fp, act = action(x, current, avgPort, price, fee)
-            avgPorts[mod] = newAvgPort
+            bleh, fp, act = action(x, current, portfolio, price, fee)
             modActs[mod] = act
-        
-        # Get average portfolio method 1
-        coin = np.mean([i[0] for i in avgPorts])
-        cash = np.mean([i[1] for i in avgPorts])
-        avgPort = (coin,cash)
-
+        # Take action
         # Get average portfolio method 2
         meanAct = np.mean(modActs)
-        avgPort2 = (avgPort[0]+meanAct, avgPort[1]-meanAct*price*(1-fee))
+        if(meanAct>0):
+            portfolio = (portfolio[0]+meanAct,portfolio[1]-meanAct*price*(1+fee))
+        else:
+            portfolio = (portfolio[0]+meanAct, portfolio[1]-meanAct*price*(1-fee))
 
-    # Add performances ** comparing wealth at end to wealth at start:
-    # (not hold wealth at end to trader wealth a end like above)
-    # update initport to be star of month? or just a sum?
+        # Update current coins and cash
+        coins = portfolio[0] 
+        cash = portfolio[1]
 
-    # Starting wealth
-    iWealth = 2*close[month*bch]
+        # Update history vector of coins, cash, wealth, and baseline wealths
+        coinss[ind-nstart] = coins
+        cashs[ind-nstart] = cash
+        wealths[ind-nstart] = coins*fp + cash
+        ogWealths[ind-nstart] = close[0]+close[ind]
 
-    # Baseline
-    base.append(price - close[month*bch])
-    lis = [por[0]*price + por[1] - iWealth for por in modPorts]
-    modperfs.append(lis)
-    average.append(avgPort[0]*price + avgPort[1]-iWealth)
-    average2.append(avgPort2[0]*price + avgPort2[1]-iWealth)
+    # Get dates of subset to return
+    dates = data.index[nstart:nfinish]
 
-# Write to csv =================================================================\
+    return(coinss, cashs, wealths, ogWealths,dates)
 
-# Baseline and averages performance
-df1 = pd.DataFrame([base,average,average2])
-df1 = df1.transpose()
-df1.columns = ["Baseline", "AveragePort", "AverageAct"]
-df1.to_csv("base&avgPerfs.csv", index=False)
+#xopts = pd.read_csv("modelParamsBTCall.csv")
 
-# Model performances
-df2 = pd.DataFrame(modperfs)
-df2.columns = ["Model"+str(i) for i in range(nmodels)]
-df2.to_csv("modPerfs.csv", index=False)
+if(toSim):
+    # each month
+    for month in test:
+        iDay = month*bch
+        coinss, cashs, wealths, ogWealths, dates = avgSim(data, xopts, (1,close[0]), iDay, iDay+30, 0.01,close)
 
-# Models
-df3 = pd.DataFrame(xopts)
-df3 = df3.transpose()
-df3.columns = ["Model"+str(i) for i in range(nmodels)]
-df3.to_csv("modelParams.csv",index=False)
+        plotVsBaseline(dates, wealths, ogWealths)
+        plotCoins(dates, coinss, close[iDay:iDay+30]) 
+    # all data, average
+    coinss, cashs, wealths, ogWealths, dates = avgSim(data, xopts, (1,close[0]), 0 , nd, 0.01,close)
+    
+    plotVsBaseline(dates, wealths, ogWealths)
+    plotCoins(dates, coinss, close[0:nd]) 
+
+    # all data, one model
+    coinss, cashs, wealths, ogWealths, dates = testSim(data, xopts.iloc[:,4].values, (1,close[0]), 0, nd,0.01)
+
+
+
+    plotVsBaseline(dates, wealths, ogWealths)
+    plotCoins(dates, coinss, close[0:nd]) 
+print("yee")
+
+def plotData():
+    data = pd.read_csv("TI_data_btc1.csv", index_col='date', parse_dates=True)
+    data.sort_index(inplace=True)
+    fig = plt.figure(figsize=(7.5,5.5))
+    ax = fig.add_subplot(111)
+
+    # make a plot
+    ax.plot(data.index,data["close"],color="red", label = "Closing Price")
+    # set x-axis label
+    ax.set_xlabel("Date")
+    # set y-axis label
+    ax.set_ylabel("Closing Price ($USD)")
+
+    ax2=ax.twinx()
+    ax2.plot(data.index, data["OBV"],"b--", label = "OBV")
+    ax2.set_ylabel("OBV")
+
+    plt.title("Closing Price and OBV of Bitcoin")
+    fig.legend(loc=("upper left"),borderaxespad=7.5)
+    plt.show()
+
+#plotData()
